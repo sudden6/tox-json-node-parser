7b957ce43bd9500fc069898707ff5d32d452f9b9
Zetok Zalbavar <zetok@openmailbox.org>
2016-09-08 19:33:14 +0100

    style: change tabs to spaces & remove trailing whitespaces

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 /*
     Copyright © 2016 sudden6 <sudden6@gmx.at>
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -22,32 +25,32 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() {
-	
-	// read data from stdin
+
+    // read data from stdin
     let mut stdin = io::stdin();
     let mut input_json = String::new();
     stdin.read_to_string(&mut input_json).unwrap();
-    
+
     // parse it to json
     let json = Json::from_str(&input_json).unwrap();
 
     // find the part that holds the bootstrap nodes
     let nodes = json.find("nodes").unwrap().as_array().unwrap();
-	
-	for node in nodes {
-		let node_obj = node.as_object().unwrap();
 
-		// get relevant parts of the node
-		let ipv4 = node_obj.get("ipv4").unwrap().as_string().unwrap();
-		let ipv6 = node_obj.get("ipv6").unwrap().as_string().unwrap();
-		let port = node_obj.get("port").unwrap();
-		let pk   = node_obj.get("public_key").unwrap().as_string().unwrap();
-		let name = node_obj.get("maintainer").unwrap().as_string().unwrap();
-		
-		println!("{} {} {} {}", ipv4, port, pk, name);
-		// also print ipv6 part if it exists
-		if ipv6 != "-" {
-			println!("{} {} {} {}", ipv6, port, pk, name);
-		}
-	}
+    for node in nodes {
+        let node_obj = node.as_object().unwrap();
+
+        // get relevant parts of the node
+        let ipv4 = node_obj.get("ipv4").unwrap().as_string().unwrap();
+        let ipv6 = node_obj.get("ipv6").unwrap().as_string().unwrap();
+        let port = node_obj.get("port").unwrap();
+        let pk   = node_obj.get("public_key").unwrap().as_string().unwrap();
+        let name = node_obj.get("maintainer").unwrap().as_string().unwrap();
+
+        println!("{} {} {} {}", ipv4, port, pk, name);
+        // also print ipv6 part if it exists
+        if ipv6 != "-" {
+            println!("{} {} {} {}", ipv6, port, pk, name);
+        }
+    }
 }
